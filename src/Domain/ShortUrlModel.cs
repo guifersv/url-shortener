0e85e38a982490aa76172dd63fe358463bcd27d5
Guilherme Fernandes <gui.fer.sv@gmail.com>
2025-10-10 15:27:21 -0300

    Fix for sqids pattern

--- src/Domain/ShortUrlModel.cs
+++ src/Domain/ShortUrlModel.cs
@@ -6,8 +6,7 @@ public class ShortUrlModel
 {
     public int Id { get; set; }
     public required string Url { get; init; }
-    [MaxLength(30), MinLength(5)]
-    public required string Alias { get; set; }
+    public string? Alias { get; set; }
     public int Accesses { get; set; }
     public DateTime DateCreated { get; init; }
 }
@@ -16,8 +15,8 @@ public record ShortUrlDto
 {
     [Url(ErrorMessage = "Invalid URL.")]
     public required string Url { get; set; }
-    [StringLength(30, MinimumLength = 5, ErrorMessage = "The alias must be greater that 5 and less than 30 characters.")]
-    public required string Alias { get; set; }
+    [StringLength(30, MinimumLength = 5, ErrorMessage = "The alias must be greater than 5 and less than 30 characters.")]
+    public string? Alias { get; set; }
     public int Accesses { get; set; }
     public DateTime DateCreated { get; set; }
 }
--- src/Migrations/20251010182643_SqidsFix.Designer.cs
+++ src/Migrations/20251010182643_SqidsFix.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UrlShortener.Infrastructure;
 
@@ -11,9 +12,11 @@ using UrlShortener.Infrastructure;
 namespace UrlShortener.Migrations
 {
     [DbContext(typeof(UrlShortenerContext))]
-    partial class UrlShortenerContextModelSnapshot : ModelSnapshot
+    [Migration("20251010182643_SqidsFix")]
+    partial class SqidsFix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
