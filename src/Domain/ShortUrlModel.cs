edf54ed0ee19e365d4ef08a6d178b2948f52ebdb
Guilherme Fernandes <gui.fer.sv@gmail.com>
2025-10-10 14:43:30 -0300

    Model prep to Sqids pattern for alias

--- src/Domain/ShortUrlModel.cs
+++ src/Domain/ShortUrlModel.cs
@@ -4,9 +4,10 @@ namespace UrlShortener.Domain;
 
 public class ShortUrlModel
 {
+    public int Id { get; set; }
     public required string Url { get; init; }
-    [MaxLength(30), Key]
-    public required string Alias { get; init; }
+    [MaxLength(30), MinLength(5)]
+    public required string Alias { get; set; }
     public int Accesses { get; set; }
     public DateTime DateCreated { get; init; }
 }
@@ -15,7 +16,7 @@ public record ShortUrlDto
 {
     [Url(ErrorMessage = "Invalid URL.")]
     public required string Url { get; set; }
-    [StringLength(30, ErrorMessage = "The alias must be less than 30 characters.")]
+    [StringLength(30, MinimumLength = 5, ErrorMessage = "The alias must be greater that 5 and less than 30 characters.")]
     public required string Alias { get; set; }
     public int Accesses { get; set; }
     public DateTime DateCreated { get; set; }
