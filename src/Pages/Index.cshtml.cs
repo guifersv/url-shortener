1409171573de52b3aac16892b5c1a6fa18246902
Guilherme Fernandes <gui.fer.sv@gmail.com>
2025-10-17 18:32:49 -0300

    CreateModel page model and html
    Might add view components to prevent over-posting and achieve ajax.

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -1,8 +1,7 @@
-using UrlShortener.Domain;
-using UrlShortener.Services.Interfaces;
-
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using UrlShortener.Domain;
+using UrlShortener.Services.Interfaces;
 
 namespace UrlShortener.Pages;
 
@@ -11,8 +10,30 @@ public class IndexModel(IUrlShortenerService service, ILogger<IndexModel> logger
     private readonly ILogger<IndexModel> _logger = logger;
     private readonly IUrlShortenerService _service = service;
 
-    public void OnGet()
-    {
+    [BindProperty]
+    public required ShortUrlDto ShortUrl { get; set; }
 
+    public void OnGet() { }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        _logger.LogDebug("IndexModel: Creating Model with binded form.");
+        if (!ModelState.IsValid)
+            return Page();
+
+        if (
+            string.IsNullOrWhiteSpace(ShortUrl.Alias)
+            || await _service.FindShortUrlModelByAlias(ShortUrl.Alias) is null
+        )
+        {
+            await _service.CreateShortUrlModel(ShortUrl);
+            return RedirectToPage();
+        }
+        else
+        {
+            _logger.LogWarning("IndexModel: Alias is not available.");
+            ModelState.AddModelError("ShortUrl.Alias", "Alias is not available.");
+            return Page();
+        }
     }
 }
